3eb179ae9258ef8b9d4e2677c072f9bea3dd4b8d
MuhammadAli206 <154820260+MuhammadAli206@users.noreply.github.com>
2026-07-04 01:36:42 +0500

    done

---
+++ js assighment 1/aap.js
@@ -0,0 +1,89 @@
+
+// Task 1 – Create Variable
+
+// String
+let name = "Muhammad Ali";
+
+// Number
+let age = 22;
+
+// Boolean
+let isStudent = true;
+
+// Array
+let colors = ["Black", "Blue", "White", "Red", "Green"];
+
+// Object
+let info = {
+  name: "Muhammad Ali",
+  age: 22,
+  city: "Karachi"
+};
+
+// Undefined
+let aboutMe;
+
+// Null
+let address = null;
+
+
+
+//  Update Variables
+
+// let variable
+let country = "Pakistan";
+country = "Saudi Arabia";
+
+// const variable
+const pi = 3.14;
+
+// pi = 3.1416;
+// Error: const variable ki value ko update nahi kar sakte.
+
+
+
+//  Print Output
+// 
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+console.log(colors);
+console.log(info);
+console.log(aboutMe);
+console.log(address);
+console.log(country);
+console.log(pi);
+
+
+
+// Check Data Types
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof colors);
+console.log(typeof info);
+console.log(typeof aboutMe);
+console.log(typeof address);
+console.log(typeof country);
+console.log(typeof pi);
+
+
+
+// Variable Naming Rules
+
+// let 1name = "Ali"; 
+let name1 = "Ali"; // 
+
+// let for = "Hello";
+let courseName = "Web Development"; 
+
+// let my-name = "Ali"; 
+let myName = "Ali"; // 
+
+// let @age = 22; 
+let userAge = 22; // 
+
+// let first name = "Ali"; 
+let firstName = "Ali"; // 
